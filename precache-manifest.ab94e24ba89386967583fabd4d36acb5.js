2842b809aa8e66c6bb16c537bbf6b3223f99595c
hamza <badfizer@gmail.com>
2021-04-11 17:30:52 +0000

    Updates

---
+++ precache-manifest.ab94e24ba89386967583fabd4d36acb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0161a09884ee52d485e1806a08361da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61b76a8c60eccb3a62c7",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "348655ee0edbd32703b8",
+    "url": "/static/css/main.300acb78.chunk.css"
+  },
+  {
+    "revision": "61b76a8c60eccb3a62c7",
+    "url": "/static/js/2.49da4047.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.49da4047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "348655ee0edbd32703b8",
+    "url": "/static/js/main.12d601a0.chunk.js"
+  },
+  {
+    "revision": "1535ba1dc7849a9b90e4",
+    "url": "/static/js/runtime-main.4220ea81.js"
+  }
+]);
